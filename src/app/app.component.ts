cd647f76245a9cae0e9a5a0bf00707ae5d29d8f5
Eduardo Sampaio <edumsamp@gmail.com>
2018-01-26 15:26:40 -0200

    primeira-versão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  menuVisivel = true;
+  dados = {titulo: '', conteudo:''};
+  lista = []
+
+  constructor(){
+    let dadosString = localStorage.getItem('cards');
+
+    if(dadosString){
+      this.lista = JSON.parse(dadosString);
+    }
+  }
+
+  alternarMenu(){
+    if(this.menuVisivel){
+      this.menuVisivel = false
+    }else{
+      this.menuVisivel = true
+    }
+  }
+
+  inserir(){
+    this.lista.push(this.dados);
+    this.dados = {titulo:'', conteudo:''};
+    console.log(this.lista);
+    let dadosString = JSON.stringify(this.lista);
+    localStorage.setItem('cards', dadosString);
+  }
+
+  limpar(){
+    this.lista = [];
+    localStorage.clear();
+  }
+
+
+
 }
